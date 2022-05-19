7aad0c8571719c99df1432dd645072b9cc69512c
Anastasia <80560435+Stasies@users.noreply.github.com>
2022-05-19 12:22:53 +0500

    sh

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/pomodoroTimer/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/pomodoroTimer/" : "/",
-};
