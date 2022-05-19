01a1071005099980a44154176b282e00f3c627d7
Anastasia <80560435+Stasies@users.noreply.github.com>
2022-05-19 12:05:31 +0500

    ds

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,3 @@
-<!-- <script setup>
-import { ref } from "vue";
-import FirstComponent from "./components/FirstComponent.vue";
-const likes = ref(0);
-const dislikes = ref(0);
-const text = ref("");
-const blue = ref("");
-const ok = true;
-const b = 0;
-const a = 0;
-const c;
-function log(a, b) {
-  c = a - b;
-  return c;
-}
-</script>
-
-<template>
-  <div>Hello, World</div>
-  <p>likes: {{ likes }}</p>
-  <p>dislikes: {{ dislikes }}</p>
-  <button @click="likes += 1">Like</button>
-  <button @click="dislikes += 1">Dislike</button>
-  <input type="text" @input="(e) => (text = e.target.value)" />
-  <input type="text" v-model="blue" />
-  <p>{{ text }}</p>
-  <p>{{ blue }}</p>
-  <input type="number" v-model.number="b" />
-  <input type="number" v-model.number="a" />
-  <button @click="log(b, a)">count</button>
-  <p v-if="blue">yes</p>
-  <p v-else>{{ c }}</p>
-  <FirstComponent />
-</template>
-
-<style></style> -->
 <script setup>
 import Navbar from "@/components/navbar/Navbar.vue";
 import Tasks from "@/components/tasks/Tasks.vue";
